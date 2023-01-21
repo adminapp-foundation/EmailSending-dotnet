c3fd37bf8cf88ab74188dbc567971ed5d4208ba1
Moe Myat Zaw <mmzliveid@gmail.com>
2023-01-21 14:47:41 +0630

    Add AdminApp.Extensions.EmailSending.Mailgun

---
+++ src/AdminApp.Extensions.EmailSending.Mailgun/src/MailgunEmailSenderProvider.cs
@@ -0,0 +1,10 @@
+﻿// Use of this source code is governed by an MIT-style license that can be found in the LICENSE.txt file at the repository root.
+
+namespace AdminApp.Extensions.EmailSending.Mailgun
+{
+    public class MailgunEmailSenderProvider : IEmailSenderProvider
+    {
+        public IEmailSender CreateEmailSender(string categoryName) => throw new NotImplementedException();
+        public void Dispose() => throw new NotImplementedException();
+    }
+}
